fcfc4eb34c10158706eac3885c0626481bc76942
Jayce Nga <jayceenga@gmail.com>
2025-04-04 18:53:35 +0800

    added tabs and notif screen

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import { Tabs } from 'expo-router'
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+        <Tabs.Screen name='index'/>
+        <Tabs.Screen name='bookmarks'/>
+        <Tabs.Screen name='create'/>
+        <Tabs.Screen name='notifications'/>
+        <Tabs.Screen name='profile'/>
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, Touchable, TouchableOpacity, View } from "react-native";
+import { styles } from "../../styles/auth.styles";
+import { Link } from "expo-router";
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Link href={"/notifications"}>Visit Notifications</Link>
+    </View>
+  );
+}
