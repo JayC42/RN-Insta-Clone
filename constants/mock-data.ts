db804183a9dc2a25378bbc317e1c970ef51bd442
Jayce Nga <jayceenga@gmail.com>
2025-04-09 15:40:46 +0800

    get and fetch post implemented

---
+++ constants/mock-data.ts
@@ -0,0 +1,51 @@
+export const STORIES = [
+    {
+        id: "1",
+        username: "You",
+        avatar: "https://avatar.iran.liara.run/public/41",
+        hasStory: false,
+    },
+    {
+        id: "2",
+        username: "brianna",
+        avatar: "https://avatar.iran.liara.run/public/54",
+        hasStory: true,
+    },
+    {
+        id: "3",
+        username: "charlie",
+        avatar: "https://avatar.iran.liara.run/public/19",
+        hasStory: true,
+    },
+    {
+        id: "4",
+        username: "diana",
+        avatar: "https://avatar.iran.liara.run/public/96",
+        hasStory: true,
+    },
+    {
+        id: "5",
+        username: "edison",
+        avatar: "https://avatar.iran.liara.run/public/46",
+        hasStory: true,
+    },
+    {
+        id: "6",
+        username: "frankie",
+        avatar: "https://avatar.iran.liara.run/public/38",
+        hasStory: true,
+    },
+    {
+        id: "7",
+        username: "gianna",
+        avatar: "https://avatar.iran.liara.run/public/57",
+        hasStory: true,
+    },
+    {
+        id: "8",
+        username: "hanna",
+        avatar: "https://avatar.iran.liara.run/public/61",
+        hasStory: true,
+    }
+]
+
